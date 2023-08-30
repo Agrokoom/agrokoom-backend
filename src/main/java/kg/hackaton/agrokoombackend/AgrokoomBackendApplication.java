114eebaab9dab10e206bbb625c79eb4cb184f1b0
Diyorka <diyorp2@mail.ru>
2023-08-30 12:45:19 +0600

    initialize project

---
+++ src/main/java/kg/hackaton/agrokoombackend/AgrokoomBackendApplication.java
@@ -0,0 +1,13 @@
+package kg.hackaton.agrokoombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrokoomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrokoomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/hackaton/agrokoombackend/AgrokoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.hackaton.agrokoombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrokoomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
